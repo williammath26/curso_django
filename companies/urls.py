b93ac11418ccb9f1d6d56ad9a7010aaefca64a74
williammath26 <matheuswilliam70@gmail.com>
2024-12-18 13:47:34 -0300

    Update urls.py

--- companies/urls.py
+++ companies/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from companies.views.employees import Employees, EmployeeDetail
 from companies.views.permissions import PermissionDetail
+from companies.views.groups import Groups ,GroupDetail
 
 urlpatterns = [
     # Lista de funcionários
@@ -10,5 +11,7 @@ urlpatterns = [
     path('employees/<int:employee_id>', EmployeeDetail.as_view(), name='employee_detail'),
     
     #Groups anda permissions anda endpoins
+    path('groups', Groups.as_view(), name='group_list'),  # Para listar os grupos
+    path('groups/<int:group_id>', GroupDetail.as_view(), name='group_detail'),  # Para acessar um grupo específico
     path('permissions',PermissionDetail.as_view())
 ]
