ef922073fef12dc6f349760ae902586bfcd0d10b
Matheus William <116811463+williammath26@users.noreply.github.com>
2024-12-18 09:59:56 -0300

    Update permissions.py

--- companies/views/permissions.py
+++ companies/views/permissions.py
@@ -1,1 +1,12 @@
 from companies.views.base import Base
+from companies.utils.permission import GroupsPermission
+from companies.serializers import PermissionSerializer
+
+from rest_framework.response import Response
+
+from django.contrib.auth.models import permission
+
+class PermissionDetail(Base):
+    permission_classes = [GroupsPermission]
+    
+    def get
